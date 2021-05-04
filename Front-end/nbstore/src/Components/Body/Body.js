266aa39bda888eaff8891e0bf27d12c5eab9f3e1
DESKTOP-4T0TPOF\Minh Nhut <nhatminh785@gmail.com>
2021-05-04 07:14:11 +0700

    - Continue Design component Product

--- Front-end/nbstore/src/Components/Body/Body.js
+++ Front-end/nbstore/src/Components/Body/Body.js
@@ -1,7 +1,7 @@
 import "./Body.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {Card, Button} from "react-bootstrap";
-import Acc_1 from "../../assets/Acc_1.jpg";
+import { Swiper, SwiperSlide } from 'swiper/react';
 export default function Body() {
   return (
     <>
@@ -22,13 +22,65 @@ export default function Body() {
         </div>
       </div>
       <div className="container mb-4 text-center">
+      <div className="row">
+
         <div className="content-body">
           <div className="group-content">
             <div className="group-sale">
               <div className="text-center">
               
                 <br/>
-              <div className="sale">
+                <Swiper
+                    spaceBetween={-515}
+                    slidesPerView={3}
+                    onSlideChange={() => console.log('slide change')}
+                    onSwiper={(swiper) => console.log(swiper)}
+                  >
+                    <SwiperSlide><div className="sale">
+                <Card style={{ width: '240px' }}>
+                  <Card.Img variant="top" src="https://cdn.cellphones.com.vn/media/catalog/product/cache/7/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/s/a/samsung-galaxy-a12_2_.jpg"/>
+                  <div className="product__box-sticker">
+                        <p className="sticker-percent">
+                          -10%
+                        </p>
+                        <p className="sticker-flashsale">
+                          <i className="icon-cps-sale"></i>
+                        </p>
+                  </div>
+                  <Card.Body>
+                    <Card.Title>
+                      <h3>Samsung galaxy A12</h3>
+                    </Card.Title>
+                    
+                    <Button variant="primary">Mua ngay</Button>
+                  </Card.Body>
+                </Card>
+                </div></SwiperSlide>
+                    <SwiperSlide><div className="sale">
+                <Card style={{ width: '240px' }}>
+                  <Card.Img variant="top" src="https://cdn.cellphones.com.vn/media/catalog/product/cache/7/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/s/a/samsung-galaxy-a12_2_.jpg"/>
+                  <Card.Body>
+                    <Card.Title>
+                      <h3>Samsung galaxy A12</h3>
+                    </Card.Title>
+                    
+                    <Button variant="primary">Mua ngay</Button>
+                  </Card.Body>
+                </Card>
+                </div></SwiperSlide>
+                    <SwiperSlide><div className="sale">
+                <Card style={{ width: '240px' }}>
+                  <Card.Img variant="top" src="https://cdn.cellphones.com.vn/media/catalog/product/cache/7/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/s/a/samsung-galaxy-a12_2_.jpg"/>
+                  <Card.Body>
+                    <Card.Title>
+                      <h3>Samsung galaxy A12</h3>
+                    </Card.Title>
+                    
+                    <Button variant="primary">Mua ngay</Button>
+                  </Card.Body>
+                </Card>
+                </div></SwiperSlide>
+                    <SwiperSlide><div className="sale">
                 <Card style={{ width: '240px' }}>
                   <Card.Img variant="top" src="https://cdn.cellphones.com.vn/media/catalog/product/cache/7/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/s/a/samsung-galaxy-a12_2_.jpg"/>
                   <Card.Body>
@@ -40,7 +92,8 @@ export default function Body() {
                   </Card.Body>
                 </Card>
                 </div>
-                <div className="sale">
+                </SwiperSlide>
+                <SwiperSlide><div className="sale">
                 <Card style={{ width: '240px' }}>
                   <Card.Img variant="top" src="https://cdn.cellphones.com.vn/media/catalog/product/cache/7/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/s/a/samsung-galaxy-a12_2_.jpg"/>
                   <Card.Body>
@@ -52,58 +105,88 @@ export default function Body() {
                   </Card.Body>
                 </Card>
                 </div>
-                <div className="sale">
-                <Card style={{ width: '240px' }}>
-                  <Card.Img variant="top" src="https://cdn.cellphones.com.vn/media/catalog/product/cache/7/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/s/a/samsung-galaxy-a12_2_.jpg"/>
-                  <Card.Body>
-                    <Card.Title>
-                      <h3>Samsung galaxy A12</h3>
-                    </Card.Title>
-                    
-                    <Button variant="primary">Mua ngay</Button>
-                  </Card.Body>
-                </Card>
-                </div>
-                <div className="sale">
-                <Card style={{ width: '240px' }}>
-                  <Card.Img variant="top" src="https://cdn.cellphones.com.vn/media/catalog/product/cache/7/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/s/a/samsung-galaxy-a12_2_.jpg"/>
-                  <Card.Body>
-                    <Card.Title>
-                      <h3>Samsung galaxy A12</h3>
-                    </Card.Title>
-                    
-                    <Button variant="primary">Mua ngay</Button>
-                  </Card.Body>
-                </Card>
-                </div>
-                <div className="sale">
-                <Card style={{ width: '240px' }}>
-                  <Card.Img variant="top" src="https://cdn.cellphones.com.vn/media/catalog/product/cache/7/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/s/a/samsung-galaxy-a12_2_.jpg"/>
-                  <Card.Body>
-                    <Card.Title>
-                      <h3>Samsung galaxy A12</h3>
-                    </Card.Title>
-                    
-                    <Button variant="primary">Mua ngay</Button>
-                  </Card.Body>
-                </Card>
-                </div>
-                <div className="sale">
-                <Card style={{ width: '240px' }}>
-                  <Card.Img variant="top" src="https://cdn.cellphones.com.vn/media/catalog/product/cache/7/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/s/a/samsung-galaxy-a12_2_.jpg"/>
-                  <Card.Body>
-                    <Card.Title>
-                      <h3>Samsung galaxy A12</h3>
-                    </Card.Title>
-                    
-                    <Button variant="primary">Mua ngay</Button>
-                  </Card.Body>
-                </Card>
-                </div>
+                </SwiperSlide>
                 
+                    ...
+                  </Swiper>
+              {/* <div className="sale">
+                <Card style={{ width: '240px' }}>
+                  <Card.Img variant="top" src="https://cdn.cellphones.com.vn/media/catalog/product/cache/7/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/s/a/samsung-galaxy-a12_2_.jpg"/>
+                  <Card.Body>
+                    <Card.Title>
+                      <h3>Samsung galaxy A12</h3>
+                    </Card.Title>
+                    
+                    <Button variant="primary">Mua ngay</Button>
+                  </Card.Body>
+                </Card>
+                </div>
+                <div className="sale">
+                <Card style={{ width: '240px' }}>
+                  <Card.Img variant="top" src="https://cdn.cellphones.com.vn/media/catalog/product/cache/7/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/s/a/samsung-galaxy-a12_2_.jpg"/>
+                  <Card.Body>
+                    <Card.Title>
+                      <h3>Samsung galaxy A12</h3>
+                    </Card.Title>
+                    
+                    <Button variant="primary">Mua ngay</Button>
+                  </Card.Body>
+                </Card>
+                </div>
+                <div className="sale">
+                <Card style={{ width: '240px' }}>
+                  <Card.Img variant="top" src="https://cdn.cellphones.com.vn/media/catalog/product/cache/7/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/s/a/samsung-galaxy-a12_2_.jpg"/>
+                  <Card.Body>
+                    <Card.Title>
+                      <h3>Samsung galaxy A12</h3>
+                    </Card.Title>
+                    
+                    <Button variant="primary">Mua ngay</Button>
+                  </Card.Body>
+                </Card>
+                </div>
+                <div className="sale">
+                <Card style={{ width: '240px' }}>
+                  <Card.Img variant="top" src="https://cdn.cellphones.com.vn/media/catalog/product/cache/7/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/s/a/samsung-galaxy-a12_2_.jpg"/>
+                  <Card.Body>
+                    <Card.Title>
+                      <h3>Samsung galaxy A12</h3>
+                    </Card.Title>
+                    
+                    <Button variant="primary">Mua ngay</Button>
+                  </Card.Body>
+                </Card>
+                </div>
+                <div className="sale">
+                <Card style={{ width: '240px' }}>
+                  <Card.Img variant="top" src="https://cdn.cellphones.com.vn/media/catalog/product/cache/7/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/s/a/samsung-galaxy-a12_2_.jpg"/>
+                  <Card.Body>
+                    <Card.Title>
+                      <h3>Samsung galaxy A12</h3>
+                    </Card.Title>
+                    
+                    <Button variant="primary">Mua ngay</Button>
+                  </Card.Body>
+                </Card>
+                </div>
+                <div className="sale">
+                <Card style={{ width: '240px' }}>
+                  <Card.Img variant="top" src="https://cdn.cellphones.com.vn/media/catalog/product/cache/7/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/s/a/samsung-galaxy-a12_2_.jpg"/>
+                  <Card.Body>
+                    <Card.Title>
+                      <h3>Samsung galaxy A12</h3>
+                    </Card.Title>
+                    
+                    <Button variant="primary">Mua ngay</Button>
+                  </Card.Body>
+                </Card>
+                </div>
+                 */}
+                 
               </div>
             </div>
           </div>
+        </div>
         </div>
       </div>
     </>
