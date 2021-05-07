5475ab2ad5c062d60a77d66ad4b3b9c2b7548f48
DESKTOP-4T0TPOF\Minh Nhut <nhatminh785@gmail.com>
2021-05-07 20:55:41 +0700

    -Done Design Landing Pages

---
+++ Front-end/nbstore/src/Component/Banner/Banner.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <>
+      <div className=" gearvn-content-section i100 mb-10" id="xxx-banner">
+        <div className="row row-margin-small">
+          <div className="col-12 col-sm-6 col-md-6 col-lg-3 col-xl-3 mb-10 col-padding-small">
+            <div className="border-rus">
+              <a href="#">
+                <img src="//theme.hstatic.net/1000026716/1000440777/14/xxxbanner1.jpg?v=19359" />
+              </a>
+            </div>
+          </div>
+          <div className="col-12 col-sm-6 col-md-6 col-lg-3 col-xl-3 mb-10 col-padding-small">
+            <div className="border-rus">
+              <a href="#">
+                <img src="//theme.hstatic.net/1000026716/1000440777/14/xxxbanner2.jpg?v=19359" />
+              </a>
+            </div>
+          </div>
+          <div className="col-12 col-sm-6 col-md-6 col-lg-3 col-xl-3 mb-10 col-padding-small">
+            <div className="border-rus">
+              <a href="#">
+                <img src="//theme.hstatic.net/1000026716/1000440777/14/xxxbanner3.jpg?v=19359" />
+              </a>
+            </div>
+          </div>
+          <div className="col-12 col-sm-6 col-md-6 col-lg-3 col-xl-3 mb-10 col-padding-small">
+            <div className="border-rus">
+              <a href="#">
+                <img src="//theme.hstatic.net/1000026716/1000440777/14/xxxbanner4.jpg?v=19359" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Banner;
