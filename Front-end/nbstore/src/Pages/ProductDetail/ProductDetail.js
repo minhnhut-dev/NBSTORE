5f22c2daa9564078871e35b5493f2a0a9391e6c3
DESKTOP-4T0TPOF\Minh Nhut <nhatminh785@gmail.com>
2021-05-09 22:06:23 +0700

    - Continue design page product detail

--- Front-end/nbstore/src/Pages/ProductDetail/ProductDetail.js
+++ Front-end/nbstore/src/Pages/ProductDetail/ProductDetail.js
@@ -1,11 +1,30 @@
-import React from 'react';
-
-function ProductDetail(props) {
-    return (
-        <div>
-            
+import React from "react";
+import Header from "../../Component/Header/Header";
+import "./ProductDetail.css";
+function ProductDetail() {
+  return (
+    <>
+      <Header />
+      <div className="noindex">
+        <div id="breadcrumb">
+          <div className="main">
+            <div className="breadcrumbs container">
+              <span className="showHere">Bạn đang ở:</span>
+              <a href="#" className="path">
+                Trang Chủ
+              </a>
+              <i className="fa fa-caret-right"></i>
+              <span>
+                <a href="#">Laptop</a>
+              </span>
+              <i className="fa fa-caret-right"></i>
+              <span className="path">Acer Nitro5</span>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </>
+  );
 }
 
 export default ProductDetail;
