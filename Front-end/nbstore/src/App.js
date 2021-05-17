ec46f7f47cc1c8dda5fb59d4b7b4dbb91f614ca0
DESKTOP-4T0TPOF\Minh Nhut <nhatminh785@gmail.com>
2021-05-18 02:17:44 +0700

    - Continue create Page Type Product

--- Front-end/nbstore/src/App.js
+++ Front-end/nbstore/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import React from "react";
 import ProductDetail from "./Pages/ProductDetail/ProductDetail";
 import Login from "./Pages/Login/Login";
+import TypeProduct from "./Pages/TypeProduct/TypeProduct";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Register from "./Pages/Register/Register";
 import NoMatch from "./Pages/NoMatch/NoMatch";
@@ -22,6 +23,9 @@ function App() {
           </Route>
           <Route path="/ProductDetail/">
             <ProductDetail />
+          </Route>
+          <Route path="/collections/">
+              <TypeProduct/>
           </Route>
           <Route path="*">
             <NoMatch />
